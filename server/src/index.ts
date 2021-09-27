9a7e371b74b6f570180b81defa6f2a75fe89452f
linc <yzlhyk@hotmail.com>
2021-09-27 12:26:24 +0800

    graphql init set up

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,36 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
+import express from "express";
 import { __prod__ } from "./constants";
-import { Post } from './entities/Post';
+import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
-
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+ 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
-  // const post = orm.em.create(Post, {title: 'my first post'});
-  // await orm.em.persistAndFlush(post);
-  // console.log("--------------sql2-------------");
-  // await orm.em.nativeInsert(Post, { title: "my first post 2"});
 
-  const posts = await orm.em.find(Post, {});
-  console.log(posts)
-}
+  const app = express();
 
-main().catch((err)=>{
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("Server started on localhost:4000");
+  });
+};
+
+main().catch((err) => {
   console.error(err);
-})
+});
