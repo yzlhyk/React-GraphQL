1eefb78efae571f55417dfc5ed19e25427b88de6
linc <yzlhyk@hotmail.com>
2021-09-27 20:18:26 +0800

    add redis package

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 React-GraphQL
+
+Redis package:
+yarn add express-session connect-redis
+yarn add -D @types/redis @types/express-session @types/connect-redis
--- server/src/index.ts
+++ server/src/index.ts
@@ -8,12 +8,27 @@ import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
- 
+import redis from 'redis';
+import session from 'express-session';
+import connectRedis from 'connect-redis';
+
+
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
-
+  
   const app = express();
+
+  const RedisStore = connectRedis(session)
+  const redisClient = redis.createClient();
+  
+  app.use(
+    session({
+      store: new RedisStore({ client: redisClient}),
+      secret: 'keyboard cat',
+      resave: false
+    })
+  )
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
