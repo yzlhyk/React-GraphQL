285b121ef90ecbba682b91d79368076a367ba13a
linc <yzlhyk@hotmail.com>
2021-09-27 11:03:15 +0800

    mikoORM init setup, gitIgnore set up

--- server/src/entities/Post.ts
+++ server/src/entities/Post.ts
@@ -1,17 +1,16 @@
-import { Entity, PrimaryKey, Property } from "@mikro-orm/core"
+import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
 
 @Entity()
 export class Post {
   @PrimaryKey()
   id!: number;
 
-  @Property()
+  @Property({ type: "date" })
   createdAt = new Date();
 
-  @Property({ onUpdate: () => new Date() })
+  @Property({ type: "date", onUpdate: () => new Date() })
   updatedAt = new Date();
 
-  @Property()
+  @Property({ type: "text" })
   title!: string;
-
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -5,10 +5,14 @@ import microConfig from "./mikro-orm.config";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
-  const post = orm.em.create(Post, {title: 'my first post'});
-  await orm.em.persistAndFlush(post);
-  console.log("--------------sql2-------------");
-  await orm.em.nativeInsert(Post, { title: "my first post 2"});
+  await orm.getMigrator().up();
+  // const post = orm.em.create(Post, {title: 'my first post'});
+  // await orm.em.persistAndFlush(post);
+  // console.log("--------------sql2-------------");
+  // await orm.em.nativeInsert(Post, { title: "my first post 2"});
+
+  const posts = await orm.em.find(Post, {});
+  console.log(posts)
 }
 
 main().catch((err)=>{
